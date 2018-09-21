a6d331519998f29de237bdd463dc80e734c85ed2
Zachary Lehmann <zlehmann@gmail.com>
2018-09-21 17:08:21 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -49,6 +49,7 @@ def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
   winter_supplies = []
   holiday_hash[:winter].each do |holiday, value|
+    binding.pry
     winter_supplies.push(value)
   end
 end
